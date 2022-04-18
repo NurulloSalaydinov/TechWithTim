70b5fcd76e4889d246222b71c1ff7da734029097
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-04-18 18:28:34 +0300

    project and app added

---
+++ tech_with_tim/tech_with_tim/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tech_with_tim project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech_with_tim.settings')
+
+application = get_asgi_application()
