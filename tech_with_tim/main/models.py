730529658d8b73a23f61d6b89ec797441f8cd8ef
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-04-18 19:37:27 +0300

    models py updated

--- tech_with_tim/main/models.py
+++ tech_with_tim/main/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+
+# Course
+class Course(models.Model):
+    course_name = models.CharField('Course name', max_length=255)
+    description = models.CharField('Description', max_length=255)
+    about = models.TextField('About')
+    contact = models.CharField('Contact', max_length=255)
+    image = models.ImageField('Choose image main (format: jpg, jpeg, png)')
+
+    def __str__(self):
+        return self.course_name
+
+    class Meta:
+        ordering = ['-id']
+
+
+# Teacher
+class Teacher(models.Model):
+    teacher_name = models.CharField('Teacher name', max_length=255)
+    description = models.CharField('Description', max_length=255)
+    about = models.TextField('About')
+    contact = models.CharField('Contact', max_length=255)
+    social_networks = models.CharField('Social networks', max_length=255)
+    image = models.ImageField('Choose image main (format: jpg, jpeg, png)', upload_to='photo/%y/%m/%d')
+
+    def __str__(self):
+        return self.teacher_name
+
+    class Meta:
+        ordering = ['-id']
+
+
+# Faq
+class Faq(models.Model):
+    question = models.CharField('Question', max_length=255)
+    answer = models.CharField('Answer', max_length=255)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        ordering = ['-id']
+
+
+# Photo
+class Photo(models.Model):
+    photo = models.ImageField('Choose image main (format: jpg, jpeg, png)', upload_to='photo/%y/%m/%d')
+
+    def __str__(self):
+        return f'Photo {self.photo}'
+
+    class Meta:
+        ordering = ['-id']
+
+
+# Contact
+class Contact(models.Model):
+    name = models.CharField('Name', max_length=255)
+    surname = models.CharField('Surname', max_length=255)
+    phone = models.CharField('Phone', max_length=255)
+    theme = models.CharField('Theme', max_length=255)
+    message = models.TextField('Message')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-id']
+
+
